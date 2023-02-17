53ce35b3ef82dfbf84753dfe2f2dd12ecf9346df
Vital24 <zynn00001@gmail.com>
2023-02-17 10:14:25 -0500

    So lost

--- create-task/Javascript/main.js
+++ create-task/Javascript/main.js
@@ -1,15 +1,45 @@
 import "../Css/style.css";
 
-// const URL = `https://opentdb.com/api.php?amount=${Amount}&category=${Catogory}&difficulty=${Difficulty}&type=multiple`;
+function catogory() {
+  let input = 30;
+  if ((input >= 10) & (input <= 32)) {
+    let Amount = input;
+    console.log(Amount);
+  } else {
+    console.log("You suck");
+  }
+  if (condition) {
+  } else {
+  }
+}
+
+catogory();
+const URL = `https://opentdb.com/api.php?amount=${Amount}&category=${Catogory}&difficulty=${Difficulty}&type=multiple`;
 
 const URLCATO = "https://opentdb.com/api_category.php";
 async function CatoInfo() {
   const CatoData = await fetch(URLCATO);
   const CatoJson = await CatoData.json();
-  console.log(CatoJson.trivia_categories);
+  // console.log(CatoJson.trivia_categories);
   let result = CatoJson.trivia_categories.map((el) => el.name);
   return result;
 }
 
 const Catogories = await CatoInfo();
 console.log(Catogories);
+
+// Catogories.forEach((el) => {
+//   i = 9;
+//   i++;
+
+//   let el = i;
+//   console.log(el);
+// });
+
+let CatoId = Catogories.map((el, Index) => {
+  // i = 9;
+  // i++;
+
+  let d = Index;
+  console.log(poo);
+});
